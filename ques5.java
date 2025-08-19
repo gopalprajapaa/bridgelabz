90a66e287e3d73c2c52c583640179c9f516df557
gopalprajapaa <gopalprajapati8909@gmail.com>
2025-08-19 11:10:03 +0530

    Add files via upload

---
+++ ques5.java
@@ -0,0 +1,12 @@
+package com.tech;
+
+public class ques5 {
+static int v=10;
+public static void main(String args[])
+{
+	ques5 obj1=new ques5();
+	obj1.v=20;
+	ques5 obj2=new ques5();
+	System.out.println(obj2.v);
+}
+}
