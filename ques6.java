561a52ff03375494f6a4deb0654f0724f3011697
gopalprajapaa <gopalprajapati8909@gmail.com>
2025-08-19 11:16:01 +0530

    Add files via upload

---
+++ ques6.java
@@ -0,0 +1,14 @@
+package com.tech;
+
+public class ques6 {
+   int value;
+   public ques6(int value)
+   {
+	   this.value=value;
+   }
+   public static void main(String args[])
+   {
+	   ques6 obj=new ques6(6);
+	   System.out.println(obj.value);
+   }
+}
