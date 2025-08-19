01c01df6b86439bc324502b3e2928861ef8c69c9
gopalprajapaa <gopalprajapati8909@gmail.com>
2025-08-19 11:33:44 +0530

    [gopal].ques9

---
+++ ques9.java
@@ -0,0 +1,13 @@
+//Create a class Account with instance variables: accountHolder, balance. Use default values. Print them without setting any values.
+package com.tech;
+
+public class ques9 {
+String accholder;
+int balance;
+public static void main(String args[])
+{
+	ques9 obj=new ques9();
+	System.out.println(obj.accholder);
+	System.out.println(obj.balance);
+}
+}
