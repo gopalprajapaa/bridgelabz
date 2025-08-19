87d069df0f9c5056702529fde9ba44abc3a495c6
gopalprajapaa <gopalprajapati8909@gmail.com>
2025-08-19 10:42:55 +0530

    Add files via upload

---
+++ ques3.java
@@ -0,0 +1,12 @@
+package com.tech;
+
+public class ques3 {
+      public static void fun1() {
+    	  int i=0;
+      }
+      public static void main(String args[])
+      {
+    	  fun1();//it will throw an error
+    	  System.out.println(i);
+      }
+}
